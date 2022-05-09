0e315f8d182d11deb13a837794f7eddb5cc412cc
Yannis Sgarra <hello@yannissgarra.com>
2022-05-09 17:39:50 +0200

    feat: EntityInterface needs Uuid

--- src/Model/EntityInterface.php
+++ src/Model/EntityInterface.php
@@ -11,9 +11,12 @@ declare(strict_types=1);
 
 namespace Webmunkeez\CQRSBundle\Model;
 
+use Symfony\Component\Uid\Uuid;
+
 /**
  * @author Yannis Sgarra <hello@yannissgarra.com>
  */
 interface EntityInterface
 {
+    public function getId(): Uuid;
 }
