2d62d80119c2d106e8b9e75bf0f4908a367285f7
Yannis Sgarra <hello@yannissgarra.com>
2022-05-26 13:41:21 +0200

    fix: event handler is not entity manager aware

--- src/Event/AbstractEventHandler.php
+++ src/Event/AbstractEventHandler.php
@@ -11,34 +11,15 @@ declare(strict_types=1);
 
 namespace Webmunkeez\CQRSBundle\Event;
 
-use Webmunkeez\CQRSBundle\Doctrine\Repository\EntityManagerAwareInterface;
-use Webmunkeez\CQRSBundle\Doctrine\Repository\EntityManagerAwareTrait;
-use Webmunkeez\CQRSBundle\Model\EntityInterface;
 use Webmunkeez\CQRSBundle\Validator\ValidatorAwareInterface;
 use Webmunkeez\CQRSBundle\Validator\ValidatorAwareTrait;
 
 /**
  * @author Yannis Sgarra <hello@yannissgarra.com>
  */
-abstract class AbstractEventHandler implements EventHandlerInterface, EntityManagerAwareInterface, ValidatorAwareInterface
+abstract class AbstractEventHandler implements EventHandlerInterface, ValidatorAwareInterface
 {
-    use EntityManagerAwareTrait;
     use ValidatorAwareTrait;
-
-    protected function persist(EntityInterface $entity): void
-    {
-        $this->entityManager->persist($entity);
-    }
-
-    protected function remove(EntityInterface $entity): void
-    {
-        $this->entityManager->remove($entity);
-    }
-
-    protected function flush(): void
-    {
-        $this->entityManager->flush();
-    }
 
     protected function validate(mixed $value): void
     {
